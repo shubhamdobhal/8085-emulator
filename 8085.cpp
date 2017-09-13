67b6182e6b6afc88fd9248058c10a83002cac2ad
shubham dobhal <shubhamdobhal10@gmail.com>
2017-09-13 20:50:49 +0530

    Update 8085.cpp

--- 8085.cpp
+++ 8085.cpp
@@ -1,139 +1,15 @@
-void jump(int address){
-	pc=address;
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	/*
+	....
+	....
+	....
+	WORK ON PROGRESS
+	....
+	....
+	....
+	return 0;	
 }
-
-void jump_cary(int address){
-	if(flag[0]==1)
-		pc=address;
-	else 
-		pc++;
-}
-
-void jump_not_cary(int address){
-	if(flag[0]==0)
-		pc=address;
-	else 
-		pc++;
-}
-
-void jump_zero(int address){
-	if(flag[3]==1)
-		pc=address;
-	else 
-		pc++;
-}
-
-void jump_not_zero(int address){
-	if(flag[3]==0)
-		pc=address;
-	else 
-		pc++;
-}
-
-void jump_positive(int address){
-	if(flag[4]==0)
-		pc=address;
-	else 
-		pc++;
-}
-
-void jump_negative(int address){
-	if(flag[4]==1)
-		pc=address;
-	else 
-		pc++;
-}
-
-void jump_parity_even(int address){
-	if(flag[1]==1)
-		pc=address;
-	else 
-		pc++;
-}
-
-void jump_parity_odd(int address){
-	if(flag[1]==0)
-		pc=address;
-	else 
-		pc++;
-}
-
-//airthmatic
-
-void add(int registers[],char operand){
-	switch(operand){
-		case B:
-			registers[0]+=registers[1];
-			break;
-		case C:
-			registers[0]+=registers[2];
-			break;
-		case D:
-			registers[0]+=registers[3];
-			break;
-		case E:
-			registers[0]+=registers[4];
-			break;
-		case H:
-			registers[0]+=registers[5];
-			break;
-		case L:
-			registers[0]+=registers[6];
-			break;
-		case M:
-			int address=registers[5]*256+registers[6];
-			registers[0]+=memory[address];
-			break;
-		default:
-			throw(exception);
-	}
-}
-
-void sub(int registers[],char operand){
-	switch(operand){
-		case B:
-			registers[0]-=registers[1];
-			break;
-		case C:
-			registers[0]-=registers[2];
-			break;
-		case D:
-			registers[0]-=registers[3];
-			break;
-		case E:
-			registers[0]-=registers[4];
-			break;
-		case H:
-			registers[0]-=registers[5];
-			break;
-		case L:
-			registers[0]-=registers[6];
-			break;
-		case M:
-			int address=registers[5]*256+registers[6];
-			registers[0]-=memory[address];
-			break;
-		default:
-			throw(exception);
-	}
-}
-
-int add_immidiate(int A,int val){
-	if(val/1000>0){
-		throw(exception);
-	}
-	val=hextodec(val);
-	A+=val;
-	return A;
-}
-
-int sub_immidiate(int A,int val){
-	if(val/1000>0){
-		throw(exception);
-	}
-	val=hextodec(val);
-	A-=val;
-	return A;
-}
-
-
