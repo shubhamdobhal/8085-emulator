4d3277b3e9afe6b2507c84dee73e9a9a4b5fb86c
shubham dobhal <shubhamdobhal10@gmail.com>
2017-10-05 16:11:04 +0530

    update arithmatic_instruction.cpp
    now mapping is used instead of array of registers and flags.

--- arithmatic_instructions.cpp
+++ arithmatic_instructions.cpp
@@ -1,103 +1,103 @@
 //airthmatic
 
-//10 ADD
-void add(int registers[],char operand){
-	switch(operand){
-		case B:
-			registers[0]+=registers[1];
-			break;
-		case C:
-			registers[0]+=registers[2];
-			break;
-		case D:
-			registers[0]+=registers[3];
-			break;
-		case E:
-			registers[0]+=registers[4];
-			break;
-		case H:
-			registers[0]+=registers[5];
-			break;
-		case L:
-			registers[0]+=registers[6];
-			break;
-		case M:
-			int address=registers[5]*256+registers[6];
-			registers[0]+=memory[address];
-			break;
-		default:
-			throw(exception);
+void check_flags(char operand){				//to check flags for dcr, inr .....,carry flag never changes
+	if(registers[operand]==0 || registers[operand]==128 || registers[operand]==-129){
+		flag['z']=1;
+		registers[operand]=0;
 	}
+	else{
+		flag['z']=0;
+	}
+	//code for checking parity and auxilarry carry.............
+}
+
+void check(){							//to check flags for add,sub........
+	if(registers['A']<0){
+		flag['s']=1;
+		if(registers['A']<(-128)){
+			flag['c']=1;		//if carry
+			registers['A']=129-registers['A'];		
+		}
+	}
+	else{	
+		flag['s']=0;
+		flag['c']=registers['A']/128;		//if carry
+		registers['A']%=128;
+	}
+	if(registers['A']==0)
+		flag['z']=1;
+	else
+		flag['z']=0;
+	//code for checking auxilarry and parity flag..............
+}
+
+//10 ADD
+
+void add(char operand){
+	if(operand=='M'){
+		int address=registers['H']*256+registers['L'];
+		registers['A']+=memory[address];
+	}
+	else
+		registers['A']+=registers[operand];
+	check();
 }
 
 //11 ADI
-int add_immidiate(int A,int val){
-	if(val/1000>0){
-		throw(exception);
-	}
-	val=hextodec(val);
-	A+=val;
-	return A;
+void add_immidiate(string val){
+	registers['A']+=hextodec(val);
+	check();
 }
  
 //12 ADC
-int add_carry(int A,char operand){
-	return add(A,operand)+1;
+int add_carry(char operand){
+	if(operand=='M'){
+		int address=registers['H']*256+registers['L'];
+		registers['A']+=memory[address]+flag['c'];
+	}
+	else
+		registers['A']+=registers[operand]+flag['c'];
+	check();
 }
 
 //13 ACI
-int add_carry_immidiate(int A,int val){
-	return add_immidiate(A,operand)+1;
+int add_carry_immidiate(char val){
+	registers['A']+=hextodec(val)+flag['c'];
+	check();
 }
 
 //14 SUB
-void sub(int registers[],char operand){
-	switch(operand){
-		case B:
-			registers[0]-=registers[1];
-			break;
-		case C:
-			registers[0]-=registers[2];
-			break;
-		case D:
-			registers[0]-=registers[3];
-			break;
-		case E:
-			registers[0]-=registers[4];
-			break;
-		case H:
-			registers[0]-=registers[5];
-			break;
-		case L:
-			registers[0]-=registers[6];
-			break;
-		case M:
-			int address=registers[5]*256+registers[6];
-			registers[0]-=memory[address];
-			break;
-		default:
-			throw(exception);
+void sub(char operand){
+	if(operand=='M'){
+		int address=registers['H']*256+registers['L'];
+		registers['A']-=memory[address];
 	}
+	else
+		registers['A']-=registers[operand];
+	check();
 }
 
 //15 SUI
-int sub_immidiate(int A,int val){
-	if(val/1000>0){
-		throw(exception);
-	}
-	val=hextodec(val);
-	A-=val;
-	return A;
+int sub_immidiate(char val){
+	registers['A']-=hextodec(val);
+	check();
 }
 
 //16 SBB
-int sub_borrow(int A,char operand){
-	return sub(A,operand)-flag[0];
+int sub_borrow(char operand){
+	if(operand=='M'){
+		int address=registers['H']*256+registers['L'];
+		registers['A']-=memory[address]-flag['c'];
+	}
+	else
+		registers['A']-=registers[operand]-flag['c'];
+	check();
 }
 
 //17 SBI
-int sub_borrow_immidiate(int A,int val){
-	return sub_immidiate(A,val)-flag[0];
+int sub_borrow_immidiate(char val){
+	registers['A']-=hextodec(val)-flag['c'];
+	check();
 }
 
 //18 LXI
@@ -109,72 +109,78 @@ void inst_LXI(int registers[],int val){
 }
 
 //19 INR
-void increment(int registers[],char operand){
-	switch(operand){
-		case A:
-			registers[0]++;
-		case B:
-			registers[1]++;
-			break;
-		case C:
-			registers[2]++;
-			break;
-		case D:
-			registers[3]++;
-			break;
-		case E:
-			registers[4]++;
-			break;
-		case H:
-			registers[5]++;
-			break;
-		case L:
-			registers[6]++;
-			break;
-		case M:
-			int address=registers[5]*256+registers[6];
-			memory[address]++;
-			break;
-		default:
-			throw(exception);
-	}
+void increment(char operand){
+	registers[operand]++;
+	check_flag(operand);
 }
 
 //20 INX { }
-
-//21 DCR
-void sub(int registers[],char operand){
-	switch(operand){
-		case A:
-			registers[0]--;
-		case B:
-			registers[1]--;
-			break;
-		case C:
-			registers[2]--;
-			break;
-		case D:
-			registers[3]--;
-			break;
-		case E:
-			registers[4]--;
-			break;
-		case H:
-			registers[5]--;
-			break;
-		case L:
-			registers[6]--;
-			break;
-		case M:
-			int address=registers[5]*256+registers[6];
-			memory[address]--;
-			break;
-		default:
-			throw(exception);
+void increase_reg_pair(char operand){
+	if(operand=='B'){
+		if(registers['C']==127){
+			registers['C']=0;
+			if(registers['B']==127)
+				registers['B']=0;
+		}
+		else
+			registers['C']++;
+	}
+	else if(operand=='D'){
+		if(registers['E']==127){
+			registers['E']=0;
+			if(registers['D']==127)
+				registers['D']=0;
+		}
+		else
+			registers['E']++;
+	}
+	else if(operand=='H'){
+		if(registers['L']==127){
+			registers['L']=0;
+			if(registers['H']==127)
+				registers['H']=0;
+		}
+		else
+			registers['L']++;
 	}
 }
 
+//21 DCR
+void sub(int registers[],char operand){
+	registers[operand]--;
+	check_flag(operand);
+}
+
 //22 DCX { }
+void decrease_reg_pair(char operand){
+	if(operand=='B'){
+		if(registers['C']==127){
+			registers['C']=0;
+			if(registers['B']==127)
+				registers['B']=0;
+		}
+		else
+			registers['C']++;
+	}
+	else if(operand=='D'){
+		if(registers['E']==127){
+			registers['E']=0;
+			if(registers['D']==127)
+				registers['D']=0;
+		}
+		else
+			registers['E']++;
+	}
+	else if(operand=='H'){
+		if(registers['L']==127){
+			registers['L']=0;
+			if(registers['H']==127)
+				registers['H']=0;
+		}
+		else
+			registers['L']++;
+	}
+}
 
 //23 DAD { }
 
